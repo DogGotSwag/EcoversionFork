3f46f8000d213081f17d557fb28aac0caf4d7e02
Axolotl++ <64795938+Leonardo-cpu@users.noreply.github.com>
2024-12-25 21:41:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,9 @@ function displayTrees(treeList) {
     const button = document.createElement('button');
     
     // Set the button text to be the name and scientific name of the tree
+    // Generar botones con el nombre y nombre científico de cada árbol
     button.textContent = `${tree.name} (Nombre científico: ${tree.scientificName})`;
+
     
     button.classList.add('tree-button'); // Añade una clase CSS para estilizar el botón.
 
